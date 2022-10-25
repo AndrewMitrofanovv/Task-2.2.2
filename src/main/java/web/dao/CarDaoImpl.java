ccf18557f33c9f3b1b65190345ab69362d2e58b7
AndrewMitrofanovv <Andrew.Mitrofanovv@yandex.ru>
2022-10-26 04:16:27 +0500

    first commit

---
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -0,0 +1,29 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class CarDaoImpl implements CarDao {
+
+    private final List<Car> carList = new ArrayList<>(Arrays.asList(
+            new Car(1, "Audi", "Black"),
+            new Car(2, "Mercedes", "Green"),
+            new Car(3, "Volkswagen", "Blue"),
+            new Car(4, "Ford", "Grey"),
+            new Car(5, "BMW", "Yellow")));
+
+    @Override
+    public List<Car> getCar(int count) {
+        if (count == 0) {
+            return carList;
+        }
+
+        return carList.stream().limit(count).collect(Collectors.toList());
+    }
+}
